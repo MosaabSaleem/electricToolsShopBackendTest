7ae54aa90ce3c7f7b970f2e4ca308a7825c0a2bd
MosaabSaleem <mmosaab2002@hotmail.com>
2025-06-18 22:07:58 +1000

    Revert "Reapply "Initial commit""
    This reverts commit f65d352a5991058f6c4c85271a5d9c0ba02733bd.

--- backend/routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { verifyUser } = require('../controllers/userController');
-
-router.post('/verify', verifyUser);
-
-module.exports = router;
